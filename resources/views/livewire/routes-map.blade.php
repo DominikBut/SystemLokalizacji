e892f4d682703d05bb6db70597183a6912193828
domixinox <61604625+domixinox@users.noreply.github.com>
2024-10-27 05:57:59 +0100

    added  info to clicking points

--- resources/views/livewire/routes-map.blade.php
+++ resources/views/livewire/routes-map.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ mobileFiltersOpen: false, sortMenuOpen: false, filterSectionsOpen: {}, label: '{{ $pojazdy[0]->Nazwa }}', }" class="p-3 pt-6">
+<div x-data="{ mobileFiltersOpen: false, sortMenuOpen: false, filterSectionsOpen: {}, label: '{{ $pojazdy[0]->Nazwa }}', }" class="p-4 pt-6">
 
 
     <div class="h-full">
@@ -44,11 +44,12 @@
                             </div>
                         </div>
                 </div>
-                <h2 class="text-2xl font-bold text-sky-950 max-w-xl md:text-2xl text-balance pt-2 truncate"><span wire:loading.remove>Pojazd: {{ $this->pojazd->Nazwa }}</span></h2>
+                <h2 class="text-2xl font-bold text-sky-950 max-w-xl text-balance pt-2 truncate"><span wire:loading.remove>Pojazd: {{ $this->pojazd->Nazwa }}</span></h2>
+                <h3 class="text-xl font-bold text-sky-80 max-w-xl text-balance truncate"><span wire:loading.remove>Trasa nr: {{ $this->pojazd->current_route }}</span></h3>
 
             </div>
             <div class="text-sm  text-cyan-800 " wire:loading.remove>
-                Zapis: {{ \Carbon\Carbon::setLocale('pl') }}{{ \Carbon\Carbon::parse(json_decode($this->dane)->points[count(json_decode($this->dane)->points) - 1]->created_at)->diffForHumans() }}
+                Zapis trasy: {{ \Carbon\Carbon::setLocale('pl') }}{{ \Carbon\Carbon::parse(json_decode($this->dane)->points[count(json_decode($this->dane)->points) - 1]->created_at)->diffForHumans() }}
             </div>
 
             <div class="flex flex-col justify-between border-y-2 border-gray-300 py-2 mt-2 space-y-2 overflow-auto max-h-[500px]" wire:loading.remove>
@@ -57,12 +58,30 @@
                     <div class="rounded flex h-full flex-col p-1">
                         <div class="flex-col rounded-t-lg {{ $index == 0 ? 'bg-lime-300' : ($index == count(json_decode($this->dane)->points) - 1 ? 'bg-blue-300' : 'bg-gray-200') }} pb-2 p-1 h-full px-2">
                             <p class="font-bold">Punkt {{ $index == 0 ? 'startowy' : ($index == count(json_decode($this->dane)->points) - 1 ? 'końcowy' : $index + 1)  }}:</p>
-                            <p class="self-center text-sm font-semibold pl-2"> {!! App\Models\Coordinates::formatCoordinates($info->lat, $info->lng) !!}</p>
+                            <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
+                                class="text-sky-950 w-4 h-4 flex-shrink-0 mr-1" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                  </svg>
+                                  <p class="self-center text-sm font-semibold">
+
+                                    {!! App\Models\Coordinates::formatCoordinates($info->lat, $info->lng) !!}
+                                </p>
+                            </div>
+
 
                         </div>
-                        <p class="rounded-bl-md text-xs text-gray-500 self-end place-content-end text-right p-1 bg-gray-100 w-full">
-                            {{ \Carbon\Carbon::setLocale('pl') }}{!! \Carbon\Carbon::parse($info->created_at) !!}
-                        </p>
+                        <div class="rounded-bl-md text-xs text-gray-500 self-end place-content-end text-right p-1 bg-gray-100 w-full flex">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor"
+                                class="text-gray-400 w-3 h-3 mr-1 self-center" viewBox="0 0 24 24">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                  {{ \Carbon\Carbon::setLocale('pl') }}{!! \Carbon\Carbon::parse($info->created_at) !!}
+
+
+                        </div>
                     </div>
 
                 @endforeach
--- resources/views/routes.blade.php
+++ resources/views/routes.blade.php
@@ -22,9 +22,62 @@
                                 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATS3eoPZF6fOo-JCpwGJSncxE7vQqD3_U&callback=initMap&libraries=marker&v=weekly"
                                 defer
                                 ></script>
+                                <div class="p-6">
+                                    <div class="flex flex-row justify-between border-y-2 border-gray-300 mt-2 py-1" wire:loading.remove>
+                                        <div class="text-lg font-bold lg:leading-8 text-cyan-800 text-balance">Punkt nr: <span id="pkt_nr"></span></div>
+                                        <div class="text-sm lg:text-base lg:leading-8 text-cyan-800 place-content-end">
+                                            Wysłano: <span id="pkt_data"></span>{{-- Ostatnia aktywność {{ \Carbon\Carbon::setLocale('pl') }}{{ $this->lokacja->created_at->diffForHumans() }} --}}
+                                        </div>{{-- <p class="font-bold">{{ App\Models\Coordinates::formatCreatedAt($this->lokacja->created_at) }}</p> --}}
+                                    </div>
+                                    <div class="py-4" wire:loading.remove>
+                                        <div class="w-full">
+                                            <div class="flex flex-col lg:flex-row rounded-lg bg-stone-100 items-center w-full shrink-0 grow-0 basis-auto shadow-md outline outline-2 outline-lime-600">
+                                                <div class="flex justify-center items-center rounded-md bg-lime-600 w-full h-full p-1 lg:p-4 lg:w-auto ring-1 ring-sky-950/10">
+                                                  <svg class="h-4 w-4 text-stone-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" >
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+                                                  </svg>
+                                                </div>
+                                                <p id="lokacja"
+                                                class="font-semibold text-base text-center sm:text-left sm:text-lg text-sky-950 lg:px-4 mx-2 lg:mx-0 pt-1 lg:pt-0 w-auto text-wrap lg:text-nowrap">
+                                                   <span id="pkt_lokacja"></span>
+                                                </p>
+                                          </div>
+                                        </div>
+                                    </div>
+                                    <div class="mx-auto lg:grid max-w-2xl grid-cols-1 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 mb-2" wire:loading.remove>
+                                        <div class="sm:col-span-2 items-center flex flex-row text-left w-full">
+                                            <dl class="space-y-4 text-sm lg:text-base leading-7 text-stone-700 w-[32rem]">
+                                              <div class="relative w-full">
+                                                  <div class=" bg-blue-100 rounded flex p-2 h-full items-center">
 
+                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-sky-950 w-6 h-6 flex-shrink-0 mr-4" stroke="currentColor" className="size-6">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                                      </svg>
+                                                      <span id="pkt_coords"></span>
+                                                      {{-- <div class="font-bold flex flex-row space-x-6"><p>{!! App\Models\Coordinates::formatCoordinates($this->lokacja->latitude, $this->lokacja->longitude) !!}</p></div> --}}
+
+                                                    </div>
+                                              </div>
+
+
+
+                                          </dl>
+
+                                        </div>
+                                      <div class="flex invisible sm:visible" wire:loading.remove>
+
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75" stroke="currentColor"
+                                          class="w-[0rem] max-w-none lg:max-xl:w-[6rem] xl:w-[10rem] text-lime-600">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
+                                          </svg>
+
+                                      </div>
+                                    </div>
+                                </div>
                             </div>
                     </div>
+
                     <div class="col-span-3 justify-between shadow-sm border sm:rounded-lg bg-white">
 
                          <!-- Navigation Links -->
@@ -169,6 +222,23 @@
 
                 return `${formattedLat} ${formattedLng.replace(/N$/, 'E').replace(/S$/, 'W')}`;
             }
+            function geocodeLatLng(geocoder, Glat, Glng) {
+                        const latlng = {
+                            lat: parseFloat(Glat),
+                            lng: parseFloat(Glng),
+                        };
+
+                        geocoder
+                            .geocode({ location: latlng })
+                            .then((response) => {
+                            if (response.results[0]) {
+                                document.getElementById("pkt_lokacja").innerText = response.results[0].formatted_address;
+                            } else {
+                                document.getElementById("pkt_lokacja").innerText = "Brak danych";
+                            }
+                            })
+                            .catch((e) => document.getElementById("pkt_lokacja").innerText = "Błąd"+ e );
+                    }
       function addMarkers(amap, points) {
         const infoWindow = new google.maps.InfoWindow();
             points.forEach((point, index) => {
@@ -224,15 +294,20 @@
                         title: `Punkt startowy` // Tooltip with the point number
                     };
                 }
+                    const geocoder = new google.maps.Geocoder();
+
 
                     // Create a regular marker for all points
                     const marker = new google.maps.Marker(markerOptions); // Create the marker
                     var localDate = new Date(point.created_at).toLocaleString('pl-PL');
                     marker.addListener("click", () => {
                         infoWindow.close();
+                        document.getElementById("pkt_nr").innerText = `${index + 1}`;
+                        document.getElementById("pkt_data").innerText = localDate;
+                        geocodeLatLng(geocoder,point.lat,point.lng);
+                        document.getElementById("pkt_coords").innerText = formatCoordinates(point.lat, point.lng);
                         infoWindow.setHeaderContent("Dane punktu "+ `${index + 1}`+":");
                         infoWindow.setContent([
-
                             formatCoordinates(point.lat, point.lng),
                             "Wysłano: "+localDate,
                         ].join("<br>"));
