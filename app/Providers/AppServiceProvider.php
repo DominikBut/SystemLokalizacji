dd297dcf69e12c9ff04f9aeea8d590887e40ec1b
domixinox <61604625+domixinox@users.noreply.github.com>
2024-11-05 05:14:46 +0100

    Didi email verification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Filament\Support\Colors\Color;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentColor;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +36,22 @@ class AppServiceProvider extends ServiceProvider
             'success' => Color::Green,
             'warning' => Color::Amber,
         ]);
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->greeting('Witaj!')
+                ->subject('Zweryfikuj swój adres email')
+                ->line('Kliknij przycisk poniżej, aby zweryfikować ten adres email.')
+                ->action('Zweryfikuj ten adres email', $url)
+                ->line('Jeżeli nie to nie ty stworzyłeś to konto, żadna dalsza czynność nie jest wymagana.');
+        });
+        ResetPassword::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->greeting('Witaj!')
+                ->subject('Resetowanie hasła')
+                ->line('Otrzymałeś ten email, ponieważ otrzymaliśmy prośbę o zresetowanie twojego hasła.')
+                ->action('Zmień hasło', $url)
+                ->line('Link do resetowania hasła wygaśnie w ciągu 60 minut.')
+                ->line('Jeżeli to nie ty zażądałeś zresetowania hasła, żadna dalsza czynność nie jest wymagana.');
+        });
     }
 }
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -4,23 +4,23 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        <div class="mb-4 text-base text-gray-600 pb-4">
+            {{ __('Zanim przejdziesz dalej, zweryfikuj swój adres e-mail, klikając na link, który do Ciebie wysłaliśmy. Jeśli nie otrzymałeś wiadomości e-mail, wyślij nową kilkając ponniżej.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
+                {{ __('Nowy link weryfikacyjny został wysłany na adres e-mail podany w ustawieniach profilu.') }}
             </div>
         @endif
 
-        <div class="mt-4 flex items-center justify-between">
+        <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
 
                 <div>
                     <x-button type="submit">
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Wyślij ponownie e-mail weryfikacyjny') }}
                     </x-button>
                 </div>
             </form>
@@ -28,15 +28,15 @@
             <div>
                 <a
                     href="{{ route('profile.show') }}"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="underline text-sm text-gray-600 font-bold hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    {{ __('Edit Profile') }}</a>
+                    {{ __('Edytuj profil') }}</a>
 
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
 
-                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ms-2">
-                        {{ __('Log Out') }}
+                    <button type="submit" class="underline font-bold text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ms-2">
+                        {{ __('Wyloguj') }}
                     </button>
                 </form>
             </div>
